0da24b05ed5b0668cf59690ffe4e29328e05b308
zhujintao <zjt@zhujintao.com>
2024-05-27 18:13:33 +0800

    add process.go file.

---
+++ go.mod
@@ -0,0 +1,28 @@
+module runc
+
+go 1.22.3
+
+require (
+	github.com/opencontainers/runc v1.2.0-rc.1.0.20240524171032-1aeefd9cbdda
+	github.com/opencontainers/runtime-spec v1.2.0
+	golang.org/x/sys v0.20.0
+)
+
+require (
+	github.com/checkpoint-restore/go-criu/v6 v6.3.0 // indirect
+	github.com/containerd/console v1.0.4 // indirect
+	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
+	github.com/cyphar/filepath-securejoin v0.2.5 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/moby/sys/mountinfo v0.7.1 // indirect
+	github.com/moby/sys/user v0.1.0 // indirect
+	github.com/mrunalp/fileutils v0.5.1 // indirect
+	github.com/opencontainers/selinux v1.11.0 // indirect
+	github.com/seccomp/libseccomp-golang v0.10.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/syndtr/gocapability v0.0.0-20200815063812-42c35b437635 // indirect
+	github.com/vishvananda/netlink v1.1.0 // indirect
+	github.com/vishvananda/netns v0.0.4 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
+)
---
+++ runc/process.go
@@ -0,0 +1,100 @@
+package runc
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/opencontainers/runc/libcontainer"
+	"github.com/opencontainers/runc/libcontainer/configs"
+	"github.com/opencontainers/runtime-spec/specs-go"
+	"golang.org/x/sys/unix"
+)
+
+func NewProcess(p specs.Process) (*libcontainer.Process, error) {
+
+	lp := &libcontainer.Process{
+		Args: p.Args,
+		Env:  p.Env,
+		// TODO: fix libcontainer's API to better support uid/gid in a typesafe way.
+		User:            fmt.Sprintf("%d:%d", p.User.UID, p.User.GID),
+		Cwd:             p.Cwd,
+		Label:           p.SelinuxLabel,
+		NoNewPrivileges: &p.NoNewPrivileges,
+		AppArmorProfile: p.ApparmorProfile,
+	}
+	if p.ConsoleSize != nil {
+		lp.ConsoleWidth = uint16(p.ConsoleSize.Width)
+		lp.ConsoleHeight = uint16(p.ConsoleSize.Height)
+	}
+
+	if p.Scheduler != nil {
+		s := *p.Scheduler
+		lp.Scheduler = &s
+	}
+
+	if p.IOPriority != nil {
+		ioPriority := *p.IOPriority
+		lp.IOPriority = &ioPriority
+	}
+
+	if p.Capabilities != nil {
+		lp.Capabilities = &configs.Capabilities{}
+		lp.Capabilities.Bounding = p.Capabilities.Bounding
+		lp.Capabilities.Effective = p.Capabilities.Effective
+		lp.Capabilities.Inheritable = p.Capabilities.Inheritable
+		lp.Capabilities.Permitted = p.Capabilities.Permitted
+		lp.Capabilities.Ambient = p.Capabilities.Ambient
+	}
+	for _, gid := range p.User.AdditionalGids {
+		lp.AdditionalGroups = append(lp.AdditionalGroups, strconv.FormatUint(uint64(gid), 10))
+	}
+	for _, rlimit := range p.Rlimits {
+		rl, err := createLibContainerRlimit(rlimit)
+		if err != nil {
+			return nil, err
+		}
+		lp.Rlimits = append(lp.Rlimits, rl)
+	}
+	return lp, nil
+
+}
+
+func createLibContainerRlimit(rlimit specs.POSIXRlimit) (configs.Rlimit, error) {
+	rl, err := strToRlimit(rlimit.Type)
+	if err != nil {
+		return configs.Rlimit{}, err
+	}
+	return configs.Rlimit{
+		Type: rl,
+		Hard: rlimit.Hard,
+		Soft: rlimit.Soft,
+	}, nil
+}
+
+var rlimitMap = map[string]int{
+	"RLIMIT_CPU":        unix.RLIMIT_CPU,
+	"RLIMIT_FSIZE":      unix.RLIMIT_FSIZE,
+	"RLIMIT_DATA":       unix.RLIMIT_DATA,
+	"RLIMIT_STACK":      unix.RLIMIT_STACK,
+	"RLIMIT_CORE":       unix.RLIMIT_CORE,
+	"RLIMIT_RSS":        unix.RLIMIT_RSS,
+	"RLIMIT_NPROC":      unix.RLIMIT_NPROC,
+	"RLIMIT_NOFILE":     unix.RLIMIT_NOFILE,
+	"RLIMIT_MEMLOCK":    unix.RLIMIT_MEMLOCK,
+	"RLIMIT_AS":         unix.RLIMIT_AS,
+	"RLIMIT_LOCKS":      unix.RLIMIT_LOCKS,
+	"RLIMIT_SIGPENDING": unix.RLIMIT_SIGPENDING,
+	"RLIMIT_MSGQUEUE":   unix.RLIMIT_MSGQUEUE,
+	"RLIMIT_NICE":       unix.RLIMIT_NICE,
+	"RLIMIT_RTPRIO":     unix.RLIMIT_RTPRIO,
+	"RLIMIT_RTTIME":     unix.RLIMIT_RTTIME,
+}
+
+func strToRlimit(key string) (int, error) {
+	rl, ok := rlimitMap[key]
+	if !ok {
+		return 0, fmt.Errorf("wrong rlimit value: %s", key)
+	}
+	return rl, nil
+
+}
