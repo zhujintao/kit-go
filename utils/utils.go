240ba016009a5a3410f6fcf8fe18f614235b15fd
zhujintao <zjt@zhujintao.com>
2024-06-14 15:59:05 +0800

    update

---
+++ utils/utils.go
@@ -0,0 +1,86 @@
+package utils
+
+import (
+	"strconv"
+)
+
+/*
+可转换的类型
+
+	 s	    t
+	 string  -> float64
+		     -> bool
+	 float64 -> float64
+	 bool	 -> bool
+*/
+func CheckNil(s interface{}) bool {
+
+	return s == nil
+
+}
+func ToType(s interface{}, t string) interface{} {
+
+	switch v := s.(type) {
+	case string:
+
+		switch t {
+		case "int32":
+			if v, err := strconv.Atoi(v); err == nil {
+				return int32(v)
+			}
+
+		case "float64":
+
+			if v, err := strconv.ParseFloat(v, 64); err == nil {
+
+				return v
+			}
+		case "float32":
+
+			if v, err := strconv.ParseFloat(v, 32); err == nil {
+
+				return float32(v)
+			}
+
+		case "bool":
+			if v, err := strconv.ParseBool(v); err == nil {
+				return v
+			}
+		case "string":
+			return v
+		}
+
+	case float64:
+
+		switch t {
+		case "float64":
+			return v
+		case "string":
+			return strconv.Itoa(int(v))
+
+		case "float32":
+
+			return float32(v)
+		}
+
+	case bool:
+		switch t {
+		case "bool":
+			return v
+		}
+	case int64:
+		switch t {
+		case "int32":
+			return int32(v)
+
+		case "string":
+			return strconv.Itoa(int(v))
+		case "float64":
+			return float64(v)
+
+		}
+
+	}
+	return nil
+
+}
