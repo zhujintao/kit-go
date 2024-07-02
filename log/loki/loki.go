4d14123141dcb72cf30feb15a8cc0c62c698d6cb
zhujintao <zjt@zhujintao.com>
2024-07-02 17:56:23 +0800

    update

--- log/loki/loki.go
+++ log/loki/loki.go
@@ -1,30 +1,15 @@
 package loki
 
 import (
-	"bufio"
-	"bytes"
 	"context"
 	"fmt"
-	"io"
 	"log/slog"
-	"net/http"
 	"path"
 	"runtime"
 	"sync"
-	"time"
 
-	"github.com/zhujintao/kit-go/log/loki/internal/buffer"
-
-	"github.com/gogo/protobuf/proto"
-	"github.com/golang/snappy"
-	"github.com/prometheus/common/config"
 	"github.com/prometheus/common/model"
-	"github.com/prometheus/prometheus/model/labels"
-
-	"github.com/grafana/dskit/flagext"
-	"github.com/grafana/loki/pkg/push"
-	"github.com/grafana/loki/v3/clients/pkg/promtail/api"
-	"github.com/grafana/loki/v3/pkg/logproto"
+	"github.com/zhujintao/kit-go/utils/buffer"
 )
 
 type Level int
@@ -119,11 +104,7 @@ func (l *LokiHandler) Handle(_ context.Context, r slog.Record) error {
 		lbs[model.LabelName("job")] = model.LabelValue(l.jobLabel)
 	}
 
-	batch := newBatch(0, api.Entry{Labels: lbs, Entry: logproto.Entry{Timestamp: r.Time, Line: buf.String()}})
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	err := newClient(l.url).sendBatch("", batch)
-	return err
+	return nil
 }
 
 func (l *LokiHandler) Enabled(_ context.Context, level slog.Level) bool {
@@ -143,106 +124,4 @@ func (l *LokiHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
 func (l *LokiHandler) WithGroup(name string) slog.Handler {
 	fmt.Println("WithGroup", name)
 	return l
-}
-
-const (
-	contentType           = "application/x-protobuf"
-	ReservedLabelTenantID = "__tenant_id__"
-)
-
-var UserAgent = fmt.Sprintf("promtail/%s", "v0.1")
-
-type client struct {
-	url    flagext.URLValue
-	client *http.Client
-}
-
-func newClient(lokiUrl string) *client {
-
-	cfg := config.HTTPClientConfig{}
-	c, err := config.NewClientFromConfig(cfg, "promtail", config.WithHTTP2Disabled())
-	if err != nil {
-
-		return nil
-	}
-	var clientURL flagext.URLValue
-	clientURL.Set(lokiUrl)
-
-	return &client{client: c, url: clientURL}
-}
-
-func (c *client) log(labels model.LabelSet, line string) {
-
-	req := logproto.PushRequest{Streams: make([]logproto.Stream, 0)}
-	req.Streams = append(req.Streams, push.Stream{})
-	buf, err := proto.Marshal(&req)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	buf = snappy.Encode(nil, buf)
-	c.send(context.Background(), "", buf)
-
-}
-
-func (c *client) Msg(line interface{}) {
-
-	labels := labels.NewBuilder(nil)
-	labels.Set("stream", "stdout")
-
-	lbs := make(model.LabelSet)
-	for _, l := range labels.Labels() {
-		lbs[model.LabelName(l.Name)] = model.LabelValue(l.Value)
-	}
-
-	batch := newBatch(0, api.Entry{Labels: lbs, Entry: logproto.Entry{Timestamp: time.Now(), Line: fmt.Sprintf("%v", line)}})
-	c.sendBatch("", batch)
-}
-
-func (c *client) sendBatch(tenantID string, batch *batch) error {
-
-	buf, _, err := batch.encode()
-	if err != nil {
-		return err
-	}
-	_, err = c.send(context.Background(), tenantID, buf)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (c *client) send(ctx context.Context, tenantID string, buf []byte) (int, error) {
-
-	ctx, cancel := context.WithTimeout(ctx, time.Second*3)
-	defer cancel()
-	req, err := http.NewRequestWithContext(ctx, "POST", c.url.String(), bytes.NewReader(buf))
-	if err != nil {
-		return -1, err
-	}
-
-	req.Header.Set("Content-Type", contentType)
-	req.Header.Set("User-Agent", UserAgent)
-
-	if tenantID != "" {
-		req.Header.Set("X-Scope-OrgID", tenantID)
-	}
-
-	resp, err := c.client.Do(req)
-	if err != nil {
-		return -1, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode/100 != 2 {
-
-		scanner := bufio.NewScanner(io.LimitReader(resp.Body, 1024))
-		line := ""
-		if scanner.Scan() {
-			line = scanner.Text()
-		}
-		err = fmt.Errorf("server returned HTTP status %s (%d): %s", resp.Status, resp.StatusCode, line)
-	}
-	return resp.StatusCode, err
 }
--- loki/loki.go
+++ loki/loki.go
@@ -1,28 +1,136 @@
 package loki
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
 	"log/slog"
-	"runtime"
+	"net/http"
+	"os"
+	"strings"
 	"time"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/grafana/dskit/flagext"
+	"github.com/grafana/loki/v3/pkg/logproto"
+	"github.com/grafana/loki/v3/pkg/util/build"
+
+	"github.com/golang/snappy"
+	"github.com/prometheus/common/config"
+	"github.com/prometheus/common/model"
+	"github.com/zhujintao/kit-go/utils/buffer"
 )
 
-//loki("http:///v3/api/push").msg("aaaa","sse","xxxx")
+const (
+	contentType  = "application/x-protobuf"
+	maxErrMsgLen = 1024
+	postPath     = "/loki/api/v1/push"
+)
+
+var UserAgent = fmt.Sprintf("promtail/%s", build.Version)
 
 type loki struct {
+	lokiURL  string
+	client   *http.Client
+	labels   model.LabelSet
+	tenantID string
 }
 
-//多租房
+func (l *loki) SetTenantID(id string) *loki {
 
-func New(url string) *loki {
-	return &loki{}
+	l.tenantID = id
+	return l
 }
 
+func NewLoki(URL string) *loki {
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	l := &loki{
+		lokiURL: URL,
+		labels:  make(model.LabelSet),
+	}
+	l.labels[model.LabelName("hostname")] = model.LabelValue(hostname)
+	var clientURL flagext.URLValue
+	err = clientURL.Set(l.lokiURL)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	if !strings.Contains(clientURL.Path, postPath) {
+		clientURL.Path = postPath
+		l.lokiURL = clientURL.String()
+	}
+
+	cfg := config.DefaultHTTPClientConfig
+	l.client, err = config.NewClientFromConfig(cfg, "loki-cli", config.WithHTTP2Disabled())
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	l.client.Timeout = 0
+
+	return l
+}
+
+func appendAttr(line *buffer.Buffer, k, v string) {
+	line.WriteString(" ")
+	line.WriteString(k)
+	line.WriteByte('=')
+	line.WriteString(v)
+}
+
+func (l *loki) Log(t time.Time, level int, message string, args ...any)
 func (l *loki) Send(message string, args ...any) {
-	var pc uintptr
-	var pcs [1]uintptr
-	runtime.Callers(3, pcs[:])
-	pc = pcs[0]
-	r := slog.NewRecord(time.Now(), slog.LevelInfo, message, pc)
-	r.Add(args...)
 
+	var line buffer.Buffer = *buffer.New()
+	r := slog.NewRecord(time.Now(), 0, message, 0)
+	appendAttr(&line, "msg", r.Message)
+	r.Add(args...)
+	r.Attrs(func(a slog.Attr) bool {
+		l.labels[model.LabelName(a.Key)] = model.LabelValue(a.Value.String())
+		appendAttr(&line, a.Key, a.Value.String())
+		return true
+	})
+
+	stream := &logproto.Stream{Labels: l.labels.String(), Entries: []logproto.Entry{{Timestamp: r.Time, Line: line.String()}}}
+	preq := logproto.PushRequest{
+		Streams: make([]logproto.Stream, 0),
+	}
+	preq.Streams = append(preq.Streams, *stream)
+	buf, err := proto.Marshal(&preq)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	buf = snappy.Encode(nil, buf)
+	req, err := http.NewRequest("POST", l.lokiURL, bytes.NewReader(buf))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Set("Content-Type", contentType)
+	req.Header.Set("User-Agent", UserAgent)
+	if l.tenantID != "" {
+		req.Header.Set("X-Scope-OrgID", l.tenantID)
+	}
+	resp, err := l.client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode/100 != 2 {
+		scanner := bufio.NewScanner(io.LimitReader(resp.Body, maxErrMsgLen))
+		line := ""
+		if scanner.Scan() {
+			line = scanner.Text()
+		}
+		fmt.Printf("server returned HTTP status %s (%d): %s", resp.Status, resp.StatusCode, line)
+	}
 }
