f1d96625a00a7346b5c13b47386834c321167ecc
zhujintao <zjt@zhujintao.com>
2024-10-23 19:28:59 +0800

    update

--- clickhouse/clickhouse.go
+++ clickhouse/clickhouse.go
@@ -1,6 +1,340 @@
 package clickhouse
 
-func ParseMySQLDDLQuery() string {
+import (
+	"fmt"
+	"math"
+	"strings"
+	"unsafe"
 
-	return ""
+	"github.com/pingcap/tidb/pkg/parser"
+	"github.com/pingcap/tidb/pkg/parser/ast"
+	"github.com/pingcap/tidb/pkg/parser/format"
+	"github.com/pingcap/tidb/pkg/parser/mysql"
+	"github.com/pingcap/tidb/pkg/parser/types"
+	_ "github.com/pingcap/tidb/pkg/types/parser_driver"
+)
+
+var mappedTypes map[string]string = map[string]string{
+
+	"tinyint":   "Int8",
+	"smallint":  "Int16",
+	"mediumint": "Int32",
+	"int":       "Int32",
+	"integer":   "Int32",
+	"bigint":    "Int64",
+	"float":     "Float32",
+	"double":    "Float64",
+	"timestamp": "DateTime",
+	"boolean":   "Bool",
+	"bit":       "UInt64",
+	"set":       "UInt64",
+	"year":      "Uint16",
+	"Time":      "Int64",
+	"date":      "Date32",
+	"geometry":  "String",
+	"varchar":   "String",
+	"char":      "String",
+	"text":      "String",
+	"decimal":   "Decimal",
+}
+
+type column struct {
+	name                      string
+	dataType                  string
+	increment                 bool
+	comment                   string
+	nullable                  bool
+	primaryKey, index, unique bool
+	precision                 int
+	scale                     int
+}
+
+type table struct {
+	schema      string
+	name        string
+	comment     string
+	colpos      map[string]int
+	columns     []*column
+	storage     string
+	versionName string
+	orders      []string
+	partition   string
+}
+
+func ParserMysqlCreateTableSql(sql string) string {
+
+	pr := parser.New()
+	stmt, err := pr.ParseOneStmt(sql, "", "")
+	if err != nil {
+		return ""
+	}
+	st := stmt.(*ast.CreateTableStmt)
+
+	t := &table{}
+	getName(t, st.Table)
+	getColumns(t, st.Cols)
+	getConstraint(t, st.Constraints)
+	getStorage(t, st.Options)
+	getOrderByPolicy((t))
+	getPartitionPolicy(t)
+
+	var sb strings.Builder
+	s := format.NewRestoreCtx(format.DefaultRestoreFlags, &sb)
+
+	s.WriteKeyWord("CREATE TABLE ")
+	if st.IfNotExists {
+		s.WriteKeyWord("IF NOT EXISTS ")
+	}
+	if !s.Flags.HasWithoutSchemaNameFlag() {
+
+		if t.schema != "" {
+			s.WriteName(t.schema)
+			s.WritePlain(".")
+		}
+	}
+	s.WriteName(t.name)
+	s.WritePlainf(" (\n")
+
+	for i, col := range t.columns {
+		if i > 0 {
+			s.WritePlainf(",\n")
+		}
+		s.WritePlain("  ")
+		s.WriteName(col.name)
+		s.WritePlain(" ")
+
+		dataType := col.dataType
+		if col.nullable {
+			dataType = "Nullable(" + dataType + ")"
+		}
+		s.WritePlain(dataType)
+		s.WritePlain(" ")
+		if col.comment != "" {
+			s.WriteKeyWord("COMMENT ")
+			s.WriteString(col.comment)
+		}
+
+	}
+	s.WritePlain(",\n")
+	s.WritePlainf("  INDEX %s %s TYPE minmax GRANULARITY 1", t.versionName, t.versionName)
+	s.WritePlainf("\n)\n")
+	s.WriteKeyWord("ENGINE ")
+	s.WritePlainf("%s(%s)", t.storage, t.versionName)
+	s.WritePlain("\n")
+
+	if t.partition != "" {
+		s.WriteKeyWord("PARTITION BY ")
+		s.WritePlain(t.partition)
+		s.WritePlain("\n")
+	}
+
+	s.WriteKeyWord("ORDER BY ")
+
+	if len(t.orders) > 0 {
+		s.WritePlain("(")
+		for i, col := range t.orders {
+
+			if i > 0 {
+				s.WritePlainf(",\n")
+			}
+			s.WritePlain(col)
+		}
+		s.WritePlain(")")
+	} else {
+		s.WritePlain("tuple()")
+	}
+	return sb.String()
+}
+
+func getName(table *table, t *ast.TableName) {
+
+	table.schema = t.Schema.String()
+	table.name = t.Name.String()
+
+}
+func getColumns(table *table, cols []*ast.ColumnDef) {
+
+	table.columns = make([]*column, len(cols))
+	table.colpos = map[string]int{}
+
+	for i, c := range cols {
+		colName := c.Name.Name.O
+		table.colpos[colName] = i
+		col := &column{
+			name:     colName,
+			nullable: true,
+		}
+
+		ft := c.Tp
+
+		col.dataType = mappedTypes[types.TypeToStr(ft.GetType(), ft.GetCharset())]
+		col.precision = types.UnspecifiedLength
+		col.scale = types.UnspecifiedLength
+
+		switch ft.GetType() {
+		case mysql.TypeEnum, mysql.TypeSet:
+			fmt.Println("(")
+			for i, e := range ft.GetElems() {
+				if i != 0 {
+				}
+				fmt.Println(e)
+			}
+			fmt.Println(")")
+		case mysql.TypeTimestamp, mysql.TypeDatetime, mysql.TypeDuration:
+			col.precision = ft.GetDecimal()
+		case mysql.TypeUnspecified, mysql.TypeFloat, mysql.TypeDouble, mysql.TypeNewDecimal:
+			col.precision = ft.GetFlen()
+			col.scale = ft.GetDecimal()
+		default:
+			//precision = ft.GetFlen()
+
+		}
+
+		for _, opt := range c.Options {
+
+			switch opt.Tp {
+
+			case ast.ColumnOptionNotNull:
+				col.nullable = false
+			case ast.ColumnOptionAutoIncrement:
+				col.increment = true
+			case ast.ColumnOptionComment:
+				col.comment = opt.Expr.(ast.ValueExpr).GetString()
+			}
+		}
+		table.columns[i] = col
+	}
+
+	sign_colName := getUniqueColumnName(table.colpos, "_sign")
+	version_colName := getUniqueColumnName(table.colpos, "_version")
+	table.columns = append(table.columns, &column{name: sign_colName, dataType: "Int8 MATERIALIZED 1"})
+	table.columns = append(table.columns, &column{name: version_colName, dataType: "UInt64 MATERIALIZED 1"})
+	table.versionName = version_colName
+}
+
+func getUniqueColumnName(cols map[string]int, prefix string) string {
+
+	isUnique := func(prefix string) bool {
+		_, ok := cols[prefix]
+		return !ok
+	}
+	if isUnique(prefix) {
+		return prefix
+	}
+
+	for index := 0; ; index++ {
+		curName := fmt.Sprintf("%s_%d", prefix, index)
+		if isUnique(curName) {
+			return curName
+		}
+	}
+}
+
+func findColumn(table *table, colName string) *column {
+	if i, ok := table.colpos[colName]; ok {
+		return table.columns[i]
+	}
+	return nil
+}
+func getConstraint(table *table, constraints []*ast.Constraint) {
+
+	for _, c := range constraints {
+
+		for _, key := range c.Keys {
+			colName := key.Column.Name.String()
+			col := findColumn(table, colName)
+			if col == nil {
+				continue
+			}
+			switch c.Tp {
+			case ast.ConstraintPrimaryKey:
+				col.primaryKey = true
+				col.nullable = false
+			case ast.ConstraintKey, ast.ConstraintIndex:
+				col.index = true
+			case ast.ConstraintUniqKey:
+				col.unique = true
+			}
+
+		}
+	}
+
+}
+
+func getStorage(table *table, opts []*ast.TableOption) {
+	table.storage = "ReplacingMergeTree"
+	for _, t := range opts {
+
+		switch t.Tp {
+		case ast.TableOptionComment:
+			table.comment = t.StrValue
+		}
+	}
+
+}
+
+func getOrderByPolicy(table *table) {
+
+	var orderbycol []string
+
+	var incrementKey []string
+	var nonincrementKey []string
+
+	for _, col := range table.columns {
+		colName := col.name
+		if col.nullable {
+			colName = "assumeNotNull(" + colName + ")"
+		}
+		if col.primaryKey || col.index || col.unique {
+			if col.increment {
+
+				if col.nullable {
+				}
+				incrementKey = append(incrementKey, colName)
+			} else {
+				nonincrementKey = append(nonincrementKey, colName)
+			}
+		}
+	}
+
+	orderbycol = append(orderbycol, nonincrementKey...)
+	orderbycol = append(orderbycol, incrementKey...)
+	table.orders = append(table.orders, orderbycol...)
+
+}
+func getPartitionPolicy(table *table) {
+
+	numbers_partition := func(column_name string, type_max_size uint) string {
+
+		if type_max_size <= 1000 {
+			return column_name
+		}
+
+		return fmt.Sprintf("intDiv(%s,%d)", column_name, type_max_size/1000)
+	}
+
+	for _, col := range table.columns {
+		if !col.primaryKey {
+			continue
+		}
+		switch col.dataType {
+		case "Date32", "DateTime":
+			table.partition = fmt.Sprintf("toYYYYMM(%s)", col.name)
+		case "Int8", "Uint8":
+			table.partition = numbers_partition(col.name, math.MaxUint8)
+		case "Int16", "Uint16":
+			table.partition = numbers_partition(col.name, math.MaxUint16)
+		case "Int32", "Uint32":
+			table.partition = numbers_partition(col.name, math.MaxUint32)
+		case "Int64", "Uint64":
+			table.partition = numbers_partition(col.name, math.MaxUint64)
+		}
+	}
+
+}
+
+func getSizwOfValueInMemory(i interface{}) int {
+
+	return int(unsafe.Sizeof(i))
+
 }
--- clickhouse/go.mod
+++ clickhouse/go.mod
@@ -2,13 +2,78 @@ module github.com/zhujintao/kit-go/clickhouse
 
 go 1.23.2
 
-require github.com/go-mysql-org/go-mysql v1.9.1
-
 require (
-	github.com/Masterminds/semver v1.5.0 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/benbjohnson/clock v1.3.5 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cloudfoundry/gosigar v1.3.6 // indirect
+	github.com/cockroachdb/errors v1.8.1 // indirect
+	github.com/cockroachdb/logtags v0.0.0-20190617123548-eb05cc24525f // indirect
+	github.com/cockroachdb/redact v1.0.8 // indirect
+	github.com/cockroachdb/sentry-go v0.6.1-cockroachdb.2 // indirect
+	github.com/coreos/go-semver v0.3.1 // indirect
+	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
+	github.com/cznic/mathutil v0.0.0-20181122101859-297441e03548 // indirect
+	github.com/danjacques/gofslock v0.0.0-20191023191349-0a45f885bc37 // indirect
+	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/btree v1.1.2 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
+	github.com/influxdata/tdigest v0.0.1 // indirect
+	github.com/jellydator/ttlcache/v3 v3.0.1 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20230326075908-cb1d2100619a // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/opentracing/basictracer-go v1.0.0 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pingcap/errors v0.11.5-0.20221009092201-b66cddb77c32 // indirect
-	github.com/siddontang/go v0.0.0-20180604090527-bdc77568d726 // indirect
-	github.com/siddontang/go-log v0.0.0-20180807004314-8d05993dda07 // indirect
+	github.com/pingcap/failpoint v0.0.0-20220801062533-2eaa32854a6c // indirect
+	github.com/pingcap/kvproto v0.0.0-20231122054644-fb0f5c2a0a10 // indirect
+	github.com/pingcap/log v1.1.1-0.20230317032135-a0d097d16e22 // indirect
+	github.com/pingcap/sysutil v1.0.1-0.20230407040306-fb007c5aff21 // indirect
+	github.com/pingcap/tidb v1.1.0-beta.0.20231124053542-069631e2ecfe // indirect
+	github.com/pingcap/tidb/pkg/parser v0.0.0-20231124053542-069631e2ecfe // indirect
+	github.com/pingcap/tipb v0.0.0-20230919054518-dfd7d194838f // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/power-devops/perfstat v0.0.0-20221212215047-62379fc7944b // indirect
+	github.com/prometheus/client_golang v1.17.0 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.44.0 // indirect
+	github.com/prometheus/procfs v0.11.1 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	github.com/shirou/gopsutil/v3 v3.23.5 // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tiancaiamao/gp v0.0.0-20221230034425-4025bc8a4d4a // indirect
+	github.com/tikv/client-go/v2 v2.0.8-0.20231116065855-46811b6ac353 // indirect
+	github.com/tikv/pd/client v0.0.0-20231116062916-ef6ba8551e52 // indirect
+	github.com/tklauser/go-sysconf v0.3.11 // indirect
+	github.com/tklauser/numcpus v0.6.0 // indirect
+	github.com/twmb/murmur3 v1.1.6 // indirect
+	github.com/uber/jaeger-client-go v2.22.1+incompatible // indirect
+	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.10 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.10 // indirect
+	go.etcd.io/etcd/client/v3 v3.5.10 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+	golang.org/x/exp v0.0.0-20230711005742-c3f37128e5a4 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/genproto v0.0.0-20231016165738-49dd2c1f3d0b // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20231016165738-49dd2c1f3d0b // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231016165738-49dd2c1f3d0b // indirect
+	google.golang.org/grpc v1.59.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 )
