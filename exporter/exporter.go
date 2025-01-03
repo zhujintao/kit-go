2e1511825f63283a613057d6197cb5ed07b09f9d
zhujintao <zjt@zhujintao.com>
2025-01-03 18:19:19 +0800

    update

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -6,7 +6,7 @@ import (
 	"maps"
 	"net/http"
 	"os"
-	"sync"
+	"reflect"
 
 	"github.com/prometheus/client_golang/prometheus"
 	promcollectors "github.com/prometheus/client_golang/prometheus/collectors"
@@ -14,14 +14,11 @@ import (
 	"github.com/urfave/cli/v3"
 )
 
-type Collector interface {
-	Exec(ch chan<- prometheus.Metric) error
-}
-
 var (
-	collectors map[string]Collector = map[string]Collector{}
-	listen     string
-	app        *cli.Command = &cli.Command{
+	collectors map[string]*Collector = map[string]*Collector{}
+
+	listen string
+	app    *cli.Command = &cli.Command{
 		Name:            "zjt_exporter",
 		HideHelpCommand: true,
 		Action: func(ctx context.Context, cli *cli.Command) error {
@@ -40,89 +37,8 @@ var (
 	}
 )
 
-type collector struct {
-	name    string
-	running map[string]bool
-	metric  *Metric
-	fn      func(metric *Metric) error
-}
-
-func (c *collector) AddFlag(flag ...cli.Flag) {
-	app.Flags = append(app.Flags, flag...)
-}
-
-// CallFunc collects data from fn logic, metric sned prometheus metric.
-func (c *collector) CallFunc(fn func(metric *Metric) error) {
-	c.fn = fn
-}
-
-func (c *collector) Exec(ch chan<- prometheus.Metric) error {
-	c.metric.ch = ch
-	return c.fn(c.metric)
-}
-
-func (c *collector) RegistryCollector() {
-
-	RegistryCollector(c.name, c)
-}
-
-func NewCollector(name string) *collector {
-
-	return &collector{
-		name:   name,
-		metric: newMetric(),
-	}
-}
-
-func RegistryCollector(collector string, c Collector, help ...string) {
-	flagName := fmt.Sprintf(collector)
-	flagHelp := ""
-	if len(help) == 1 {
-		flagHelp = help[0]
-	}
-	flag := &cli.BoolFlag{Name: flagName, Category: "collectors:", Usage: flagHelp, HideDefault: true}
-	flag.Action = func(ctx context.Context, cli *cli.Command, b bool) error {
-		if b {
-			collectors[collector] = c
-		}
-		return nil
-	}
-
-	app.Flags = append(app.Flags, flag)
-
-}
-func RegistryFactory(collector string, factory func() (Collector, error), help ...string) {
-
-	if factory == nil {
-		return
-	}
-
-	c, err := factory()
-	if err != nil {
-		return
-	}
-
-	flagName := fmt.Sprintf(collector)
-	flagHelp := ""
-	if len(help) == 1 {
-		flagHelp = help[0]
-	}
-
-	flag := &cli.BoolFlag{Name: flagName, Category: "collectors:", Usage: flagHelp, HideDefault: true}
-	flag.Action = func(ctx context.Context, cli *cli.Command, b bool) error {
-		if b {
-			collectors[collector] = c
-		}
-		return nil
-	}
-
-	app.Flags = append(app.Flags, flag)
-
-}
-
 type exporter struct {
-	name   string
-	metric *Metric
+	name string
 }
 
 func NewApp(appName ...string) *exporter {
@@ -147,43 +63,87 @@ func NewApp(appName ...string) *exporter {
 	for c := range maps.Keys(collectors) {
 		fmt.Printf("\t%s\n", c)
 	}
-	return &exporter{name: app.Name, metric: newMetric()}
+	return &exporter{name: app.Name}
 }
 
-func (exporter) Describe(ch chan<- *prometheus.Desc) {}
-func (e *exporter) Collect(ch chan<- prometheus.Metric) {
-	e.metric.ch = ch
+type Collector struct {
+	name   string
+	metric *Metric
+	fn     func(metric *Metric) error
+}
 
-	e.metric.Create(e.name, "up").SendWithoutNs(prometheus.GaugeValue, 1)
-	var wg sync.WaitGroup
-	for name, c := range collectors {
+// "github.com/urfave/cli/v3"
+func (c *Collector) AddFlag(flag ...cli.Flag) {
 
-		wg.Add(1)
-		go func(name string, c Collector) {
-			defer wg.Done()
-			err := c.Exec(ch)
-			if err != nil {
-				fmt.Println(c, err)
-				return
-			}
-		}(name, c)
-
+	for _, f := range flag {
+		_f := reflect.ValueOf(f).Elem().FieldByName("Name")
+		_f.Set(reflect.ValueOf(c.name + "-" + _f.String()))
 	}
-	wg.Wait()
+
+	app.Flags = append(app.Flags, flag...)
 
 }
-func (e *exporter) Namespace(n string) *exporter {
-	namespace = n
-	return e
+func (Collector) Describe(ch chan<- *prometheus.Desc) {}
+func (c *Collector) Collect(ch chan<- prometheus.Metric) {
+
+	c.metric.ch = ch
+	if c.fn == nil {
+		return
+	}
+	c.fn(c.metric)
 }
+func (c *Collector) CallFunc(fn func(metric *Metric) error) {
+	c.fn = fn
+}
+
+func NewCollector(name string) *Collector {
+
+	c := &Collector{name: name, metric: newMetric()}
+
+	return c
+}
+
+func RegCollector(fs ...func() *Collector) {
+	for _, f := range fs {
+		c := f()
+		c.RegCollector()
+	}
+}
+
+func (c *Collector) RegCollector() {
+	registry(c.name, c)
+}
+
 func (e *exporter) Run() {
 	prometheus.Unregister(promcollectors.NewGoCollector())
 	prometheus.Unregister(promcollectors.NewProcessCollector(promcollectors.ProcessCollectorOpts{PidFn: func() (int, error) {
 		return os.Getpid(), nil
 	}}))
-	prometheus.MustRegister(e)
-	http.Handle("/metrics", promhttp.Handler())
+	for _, c := range collectors {
+		prometheus.MustRegister(c)
+	}
 
+	http.Handle("/metrics", promhttp.Handler())
 	fmt.Println("Starting "+e.name, "listen", listen)
 	http.ListenAndServe(listen, nil)
+}
+
+func registry(collector string, c *Collector, help ...string) {
+
+	flagName := fmt.Sprintf(collector)
+	flagHelp := ""
+	if len(help) == 1 {
+		flagHelp = help[0]
+	}
+
+	flag := &cli.BoolFlag{Name: flagName, Category: "collectors:", Usage: flagHelp, HideDefault: true}
+	flag.Action = func(ctx context.Context, cli *cli.Command, b bool) error {
+		if b {
+			collectors[collector] = c
+		}
+		return nil
+	}
+
+	app.Flags = append(app.Flags, flag)
+
 }
