40a9fcaba89e360dcb714a82e942a46e911e29a5
zhujintao <zjt@zhujintao.com>
2025-02-20 11:12:36 +0800

    update

---
+++ jq/go.mod
@@ -0,0 +1,7 @@
+module github.com/zhujintao/kit-go/jq
+
+go 1.24.0
+
+require github.com/itchyny/gojq v0.12.17
+
+require github.com/itchyny/timefmt-go v0.1.6 // indirect
---
+++ jq/jq.go
@@ -0,0 +1,100 @@
+package jq
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"unicode"
+
+	"github.com/itchyny/gojq"
+)
+
+func PickJson(text string, jsonPath string) string {
+	var runePath []rune
+	if strings.TrimSpace(jsonPath) == "{}" {
+		jsonPath = ""
+		runePath = append(runePath, '.')
+	}
+
+	for _, r := range jsonPath {
+
+		if unicode.IsSpace(r) {
+			continue
+		}
+		if r == '{' {
+			continue
+		}
+		if r == '}' {
+			continue
+		}
+		if r == '"' {
+			continue
+		}
+		runePath = append(runePath, r)
+	}
+
+	jsonPath = string(runePath)
+	if jsonPath == "." {
+		return parserJson(text, ".")
+	}
+	sl := strings.Split(jsonPath, ":")
+	var sb strings.Builder
+	sb.WriteString(".")
+
+	if len(sl) > 1 {
+
+		var jpath string
+
+		for i, v := range sl[:len(sl)-1] {
+
+			jpath = jpath + "." + v
+			if i == 0 {
+				jpath = v
+			}
+			sb.WriteString(fmt.Sprintf(`|=with_entries(select(.key == "%s"))|.%s`, v, jpath))
+		}
+	}
+
+	keys := strings.Split(sl[len(sl)-1], ",")
+	var key string
+	for i, v := range keys {
+		kv := `"` + v + `"`
+		key = key + "," + kv
+		if i == 0 {
+			key = kv
+		}
+	}
+	sb.WriteString(fmt.Sprintf(`|=with_entries(select(.key | IN (%s)))`, key))
+	return parserJson(text, sb.String())
+
+}
+
+func parserJson(input, expression string) string {
+	var obj map[string]any
+	var s string
+	json.Unmarshal([]byte(input), &obj)
+	query, err := gojq.Parse(expression)
+	if err != nil {
+		fmt.Println("Parse", err)
+		return ""
+	}
+	iter := query.Run(obj)
+	for {
+		v, ok := iter.Next()
+		if !ok {
+			break
+		}
+		if err, ok := v.(error); ok {
+			fmt.Println("v.(error)", err)
+			break
+		}
+		sv, err := json.Marshal(v)
+		if err != nil {
+			fmt.Println("json.Marshal", err)
+			break
+		}
+		s = string(sv)
+
+	}
+	return s
+}
