3bc0e33659fdea46417c8cb1727f36fa000d6a8b
zhujintao <zjt@zhujintao.com>
2025-08-07 15:06:28 +0800

    update

--- promwrite/promwrite.go
+++ promwrite/promwrite.go
@@ -1,11 +1,12 @@
 package promwrite
 
 import (
-	"slices"
-	"sort"
+	"errors"
+	"fmt"
 	"time"
 
 	"github.com/golang/snappy"
+	"github.com/prometheus/prometheus/model/labels"
 	writev2 "github.com/prometheus/prometheus/prompb/io/prometheus/write/v2"
 	"github.com/prometheus/prometheus/storage/remote"
 	"resty.dev/v3"
@@ -15,98 +16,100 @@ type WriteRequest = writev2.Request
 type HttpRequest = resty.Request
 
 type metric struct {
-	labelName  []string
-	labelValue []string
-	labels     map[string]int
-	timeSeries []writev2.TimeSeries
+	cli *HttpRequest
+}
+type label struct {
+	m       *metric
+	name    string
+	uint    string
+	help    string
+	s       writev2.SymbolsTable
+	lables  labels.ScratchBuilder
+	samples []writev2.Sample
 }
 
-var cli *resty.Client = resty.New().SetDisableWarn(true)
+func NewMetric(url string, auth ...string) *metric {
 
-// use resty Post() write remote /api/v1/write
-func (m *metric) Send() *HttpRequest {
-	symbols := append(m.labelName, m.labelValue...)
-	w := WriteRequest{}
-	w.Symbols = append(w.Symbols, symbols...)
+	cli := resty.New()
+	if len(auth) == 2 {
+		cli.SetBasicAuth(auth[0], auth[1])
+	}
 
-	w.Timeseries = append(w.Timeseries, m.timeSeries...)
-
-	data, _ := w.Marshal()
-	return cli.R().
-		//SetBasicAuth("xxx", "xxx").
-		SetBody(snappy.Encode(nil, data)).
+	s := cli.R().
 		SetHeader("Content-Type", "application/x-protobuf;proto=io.prometheus.write.v2.Request").
 		SetHeader("Content-Encoding", "snappy").
 		SetHeader(remote.RemoteWriteVersionHeader, remote.RemoteWriteVersion20HeaderValue).
-		SetHeader("User-Agent", "promwrite/0.0.0")
+		SetHeader("User-Agent", "promwrite/0.0.0").
+		SetURL(url)
+
+	return &metric{cli: s}
 
 }
 
-type label struct {
-	m   *metric
-	ref []uint32
-}
+func (l *label) Label(name, value string) *label {
 
-func (l *label) Label(k, v string) *label {
-
-	if !slices.Contains(l.m.labelName, k) {
-		l.m.labelName = append(l.m.labelName, k)
-	}
-
-	if !slices.Contains(l.m.labelValue, v) {
-
-		l.m.labelValue = append(l.m.labelValue, v)
-
-		l.m.labels[k] = len(l.m.labelValue) - 1
-
-	}
+	l.lables.Add(name, value)
 
 	return l
 }
-
-func (l *label) SetValue(value float64, ts ...int64) {
-	sort.Strings(l.m.labelName)
-
-	for kref, s := range l.m.labelName {
-		if kref == 0 {
-			continue
-		}
-
-		vref := l.m.labels[s] + len(l.m.labelName)
-		l.ref = append(l.ref, uint32(kref), uint32(vref))
-
-	}
+func (l *label) SetValue(value float64, ts ...int64) *label {
 	_ts := time.Now().UnixMilli()
+
 	if len(ts) == 1 {
 		_ts = ts[0]
 	}
+	l.samples = append(l.samples, writev2.Sample{Value: value, Timestamp: _ts})
 
-	timeSeries := writev2.TimeSeries{LabelsRefs: l.ref}
-	timeSeries.Samples = append(timeSeries.Samples, writev2.Sample{Value: value, Timestamp: _ts})
-	l.m.timeSeries = append(l.m.timeSeries, timeSeries)
-
-	l.ref = nil
+	return l
 
 }
+func (l *label) Send() error {
 
-func (m *metric) Name(name, unit string) *label {
+	l.lables.Add("__name__", l.name)
+	l.lables.Sort()
+
+	uint := l.s.Symbolize(l.uint)
+	help := l.s.Symbolize(l.help)
+
+	w := &WriteRequest{}
+	w.Symbols = l.s.Symbols()
+	w.Timeseries = []writev2.TimeSeries{{
+		LabelsRefs: l.s.SymbolizeLabels(l.lables.Labels(), nil),
+		Samples:    l.samples,
+		Metadata:   writev2.Metadata{HelpRef: help, UnitRef: uint},
+	}}
+
+	data, _ := w.Marshal()
+	l.s.Reset()
+	l.lables.Reset()
+	fmt.Println(l.samples, len(l.samples))
+	l.samples = l.samples[:0]
+	fmt.Println(l.samples, len(l.samples))
+	resutl, err := l.m.cli.SetBody(snappy.Encode(nil, data)).Post(l.m.cli.URL)
+	if err != nil {
+		return err
+	}
+	if resutl.StatusCode() != 204 {
+		fmt.Println(resutl.String())
+		return errors.New(resutl.String())
+
+	}
+	return nil
+}
+
+func (m *metric) Name(name, unit string, help ...string) *label {
 
 	_name := name
 	if unit != "" {
 		_name = name + "_" + unit
 	}
 
-	if !slices.Contains(m.labelValue, _name) {
-		m.labelValue = append(m.labelValue, _name)
-
-	}
-
 	return &label{
-		m: m,
+		m:      m,
+		s:      writev2.NewSymbolTable(),
+		lables: labels.ScratchBuilder{},
+		name:   _name,
+		uint:   unit,
 	}
-}
 
-// Remote Write 2.0
-func NewMetric() *metric {
-	return &metric{labelName: []string{"", "__name__"}, labels: map[string]int{"__name__": 0}}
 }
