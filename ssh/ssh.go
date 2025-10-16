f09281e48bedbcf8ebb1af5f519a67a39ed38646
zhujintao <zjt@zhujintao.com>
2025-10-16 23:33:59 +0800

    update

--- ssh/ssh.go
+++ ssh/ssh.go
@@ -4,7 +4,7 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-type Conn = ssh.Conn
+type Client = ssh.Client
 
 func NewConn(addr string, user, password string) (*ssh.Client, error) {
 	return ssh.Dial("tcp", addr, &ssh.ClientConfig{User: user, Auth: []ssh.AuthMethod{ssh.Password(password)}, HostKeyCallback: ssh.InsecureIgnoreHostKey()})
